53c00ca37913df7a8ac4ed65a57294df0ec32b98
Dusan Klinec <dusan.klinec@gmail.com>
2016-12-22 00:53:15 +0100

    v0.1.4

--- pytss/__about__.py
+++ pytss/__about__.py
@@ -32,7 +32,7 @@ __all__ = [
 __title__ = "pytss"
 __summary__ = "Python binding to libtspi"
 
-__version__ = "0.1.3"
+__version__ = "0.1.4"
 
 __author__ = "Matthew Garrett"
 __email__ = "matthew.garrett@nebula.com"
